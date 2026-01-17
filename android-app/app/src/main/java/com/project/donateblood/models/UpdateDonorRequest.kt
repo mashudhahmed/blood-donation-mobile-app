fd9aea15c638020bb135e882bb91f40597d62f00
Mashudh Ahmed <22-47811-2@student.aiub.edu>
2026-01-17 16:04:54 +0600

    Remove android-app for clean reupload

--- android-app/app/src/main/java/com/project/donateblood/models/UpdateDonorRequest.kt
+++
@@ -1,9 +0,0 @@
-package com.project.donateblood.models
-
-data class UpdateDonorRequest(
-    val bloodGroup: String,
-    val district: String,
-    val lastDonationDate: String,
-    val isAvailable: Boolean,
-    val fcmToken: String
-)
